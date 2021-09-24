d7bccd717570793e703a9ac1623194990aacba94
UmanshSarabhai <49119948+UmanshSarabhai@users.noreply.github.com>
2021-09-24 14:36:59 +0530

    Update LoginButton.js

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Button from 'react-bootstrap/Button';
+import '../App.css'
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
     !isAuthenticated && (
-      <Button variant="outline-primary" size="lg"onClick={() => loginWithRedirect()}>
+      <Button variant="outline-primary" size="lg"onClick={() => loginWithRedirect()} >
         Log In
       </Button>
     )
