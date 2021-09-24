bac1287f322e1ac96549d05cd3ab595302ba68c2
UmanshSarabhai <49119948+UmanshSarabhai@users.noreply.github.com>
2021-09-24 14:34:53 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
+import DisplayAllPosts from './DisplayAllPosts';
+
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const { isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && ( 
-     <div>
-        <h2>{user.name}</h2>
-      </div>
+    <>
+      <DisplayAllPosts />
+    </>    
+    
+    
     )
+    
   )
 }
 
